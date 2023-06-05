ee7101b89f6cc4bb9fccd726384add724a5836e5
Amit lavania <amit.lavania_cs20@gla.ac.in>
2023-06-06 00:23:06 +0530

    basic

---
+++ Arrays/Largestnumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Largestnumber {
+    public static  int largest(int numbers[])
+    {
+        int largestnum=Integer.MIN_VALUE;
+        for(int i =0;i<numbers.length;i++)
+        {
+            if(largestnum<numbers[i])
+            {
+                largestnum=numbers[i];
+            }    
+        }
+        return largestnum;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,7,2,6,5,4,3};
+        System.out.println("Largest value is:"+largest(numbers));    
+    }
+    
+}
