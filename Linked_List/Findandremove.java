1a073831d9cdd7a24d8bd8bc64ffdf7e3f4eb9b6
Amit lavania <amit.lavania_cs20@gla.ac.in>
2023-08-18 01:46:07 +0530

    basic

--- Linked_List/Findandremove.java
+++ Linked_List/Findandremove.java
@@ -1,11 +1,13 @@
-public class Reversell {
-    public static class Node {
+public class Findandremove {
+
+    public static class Node
+    {
         int data;
         Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
+        public  Node(int data)
+        {
+            this.data=data;
+            this.next=null;
 
         }
     }
@@ -67,21 +69,35 @@ public class Reversell {
         System.out.println("null");
     }
 
-    public void reverse() {
-        Node prev = null;
-        Node curr = tail = head;
-        Node next;
-        while (curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+    public void deletenthfromend(int n)
+    {
+        int size=0;
+        Node temp=head;
+        while(temp!=null)
+        {
+            temp=temp.next;
+            size++;
         }
-        head = prev;
-    }
+        if(n==size)
+        {
+            head=head.next; //remove first
+            return;
+        }
+        int i=1;
+        int itofind=size-n;
+        Node prev=head;
+        while(i<itofind)
+        {
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return;
 
+        
+    } 
     public static void main(String[] args) {
-        Reversell ll = new Reversell();
+        Findandremove ll = new Findandremove();
         ll.addFirst(2);
         ll.addFirst(1);
         ll.addLast(4);
@@ -89,7 +105,9 @@ public class Reversell {
         // ll.addLast(5);
         ll.add(2, 3);
         ll.print();
-        ll.reverse();
+        ll.deletenthfromend(4);
         ll.print();
+        
     }
+    
 }
