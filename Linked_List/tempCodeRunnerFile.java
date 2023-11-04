a1de537763a8d1fbbc18cf123edbf50811f7e97d
Amit lavania <amit.lavania_cs20@gla.ac.in>
2023-11-04 14:51:52 +0530

    basic

---
+++ Linked_List/tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+ // public void print() {
+    //     if (head == null) {
+    //         System.out.println("LL is empty");
+    //         return;
+    //     }
+    //     Node temp = head;
+    //     while (temp != null) {
+    //         System.out.print(temp.data + "-->");
+    //         temp = temp.next;
+    //     }
+    //     System.out.println("null");
+    // }
